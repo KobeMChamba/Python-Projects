257d8577301f61fd9e90bb297ed332cf81b0a295
Kobe Chamba <77932095+KobeMChamba@users.noreply.github.com>
2021-08-22 11:15:48 -0400

    Add files via upload

---
+++ Summer_Practice_2021/Tuples_Practice.py
@@ -0,0 +1,6 @@
+# Similar to list but they are immutable
+# Use parentheses not square brackets
+numbers = (1, 2, 3)
+# print(numbers.count(3))
+# print(numbers[0])
+# Tuples are useful when you don't want to change content
