1e27241d7dafdb4060e50a927616fc9e805f7875
Kobe Chamba <kchamba01@gmail.com>
2023-04-06 23:46:13 -0400

    No Prefix Set: Trie implementation, passed partly

--- Spring Practice 2023/No Prefix Set.py
+++ Spring Practice 2023/No Prefix Set.py
@@ -12,50 +12,43 @@ import sys
 # The function accepts STRING_ARRAY words as parameter.
 #
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+        current.is_end_of_word = True
+        
+    def search(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                return False
+            current = current.children[char]
+            if current.is_end_of_word:
+                return True
+        return False
+
 def noPrefix(words):
-    # Write your code here
-    for i in range(len(words)):
-        for j in range(0, i):
-            #print("I: ", words[i])
-            #print("J: ", words[j])
-            
-            prefix = False
-            
-            if (len(words[i]) <= len(words[j])):
-                for letter_index in range(len(words[i])):
-                    #print("words[i][letter_index]:", words[i][letter_index])
-                    #print("words[j]:", words[j])
-                    #print("letter index:", letter_index)
-                    #print("words[j][letter_index]: ", end ='')
-                    #print(words[j][letter_index])
-                    if (words[i][letter_index] != words[j][letter_index]):
-                        prefix = False
-                        #print("They differ at letter index: ", letter_index)
-                        break
-                        
-                    else:
-                        prefix = True
-                        continue
-                if prefix:
-                    print("BAD SET")
-                    #print(words[i])
-                    print(words[i])
-                    return
-            else:
-                for letter_index in range(len(words[j])):
-                    if (words[i][letter_index] != words[j][letter_index]):
-                        prefix = False
-                        #print("They differ at letter index: ", letter_index)
-                        break
-                    else:
-                        prefix = True
-                        continue
-                if prefix:
-                    print("BAD SET")
-                    #print(words[i])
-                    print(words[i])
-                    return
+    trie = Trie()
+    for word in words:
+        if trie.search(word):
+            print("BAD SET")
+            print(word)
+            return
+        trie.insert(word)
     print("GOOD SET")
+
                     
                     
                         
