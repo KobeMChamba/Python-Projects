0cbf831e7d5946dc4c31dd970b666df462e19f53
Kobe Chamba <kchamba01@gmail.com>
2023-04-22 16:34:07 -0400

    Completed Lonely Integer.py

---
+++ Spring Practice 2023/Lonely Integer.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    count = {}
+    for num in a:
+        if num in count:
+            count[num] += 1
+        else:
+            count[num] = 1
+    # print(count)
+    for num, count in count.items():
+        if count == 1:
+            return num
+    return None
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
