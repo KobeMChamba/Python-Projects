f362e6e6fa17aacb3234f5f9df00c5fa1bd0b4de
Kobe Chamba <kchamba01@gmail.com>
2023-04-23 15:14:15 -0400

    Completed Flipping Bits.py

---
+++ Spring Practice 2023/Flipping Bits.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    # Write your code here
+    bin_num = bin(n)[2:].zfill(32)
+    table = bin_num.maketrans("01", "10")
+    str2 = bin_num.translate(table)
+    decimal_int = int(str2, 2)
+    return decimal_int
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
