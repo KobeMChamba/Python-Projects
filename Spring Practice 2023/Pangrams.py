300d534bf80af41797a207740e7ab15b8ab22769
Kobe Chamba <kchamba01@gmail.com>
2023-04-28 22:25:35 -0400

    Completed Pangrams.py

---
+++ Spring Practice 2023/Pangrams.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    s = s.replace(" ", "").lower()
+    letter_dict = {}
+    for letter in s:
+        if letter not in letter_dict:
+            letter_dict[letter] = 1
+    print(letter_dict)
+    if len(letter_dict) == 26:
+        return "pangram"
+    else:
+        return "not pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
