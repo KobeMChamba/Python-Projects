e167768c34f8ad2790e5e82234680ab09657484e
Kobe Chamba <kchamba01@gmail.com>
2023-05-17 16:44:46 -0400

    Completed Iterative implementation

---
+++ Spring Practice 2023/Reverse A Linked List Iter.py
@@ -0,0 +1,98 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'reverse' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def reverse(llist):
+    # Iterative approach
+    # Initialize pointers
+    #print(llist)
+    #print(llist.data)
+    #print(llist.next.next.next.next.next)
+    #print(llist.head)
+    #print(llist.tail)
+
+    my_list = SinglyLinkedList()
+    my_list.head = llist
+    prev = None
+    current = my_list.head
+    print(current)
+    while (current is not None):
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    my_list.head = prev
+    #print("mylisthead: ", my_list.head)
+    return my_list.head
+
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
