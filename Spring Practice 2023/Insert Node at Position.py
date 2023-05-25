43f64876bb7bfb5a297fcdc99aadccb299683bab
Kobe Chamba <kchamba01@gmail.com>
2023-05-25 14:26:03 -0400

    Completed inserting node at a position

---
+++ Spring Practice 2023/Insert Node at Position.py
@@ -0,0 +1,85 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'insertNodeAtPosition' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST llist
+#  2. INTEGER data
+#  3. INTEGER position
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def insertNodeAtPosition(llist, data, position):
+    # Write your code here
+    #print(llist)
+    #print(llist.data)
+    #print(llist.next)
+    
+    prev = None
+    pointer = llist
+    #transverse
+    while position > 0:
+        prev = pointer
+        pointer = pointer.next
+        position = position - 1
+    
+    # part that inserts data
+    # create node
+    node = SinglyLinkedListNode(data)
+    # if there is a previous node, make sure we connect it to our new node
+    if prev != None:
+        prev.next = node
+    # connect node to remainder of list
+    node.next = pointer
+    
+    return llist
+
+    
+
+if __name__ == '__main__':
