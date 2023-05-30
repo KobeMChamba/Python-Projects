99c52e55130c4e92d5213896cf5c7651eb127bc3
Kobe Chamba <kchamba01@gmail.com>
2023-05-29 20:14:22 -0400

    Completed Merge Sorted Lists.py

---
+++ Spring Practice 2023/Merge Sorted Lists.py
@@ -0,0 +1,67 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the mergeLists function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def mergeLists(head1, head2):
+    mlist = SinglyLinkedListNode(0)
+    tail = mlist
+    while head1 and head2:
+        if head1.data <= head2.data:
+            tail.next = head1
+            head1 = head1.next
+        else:
+            tail.next = head2
+            head2 = head2.next
+        tail = tail.next
+        
+    # Append any remaining nodes from either list
+    tail.next = head1 if head1 else head2
+    return mlist.next
+
+    
+
+if __name__ == '__main__':
