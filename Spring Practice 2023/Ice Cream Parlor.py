d2f0d6df7064502baef4628965894d8d3d5977a0
Kobe Chamba <kchamba01@gmail.com>
2023-05-31 01:03:48 -0400

    First attempt at Ice Cream Parlor

---
+++ Spring Practice 2023/Ice Cream Parlor.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    complement_map = {}  # dictionary to store complement of elements
+    for num in arr:
+        complement = m - num
+        if complement in complement_map:
+            return [complement, num]
+        else:
+            complement_map[num] = True
+
+    return []  # no pair found
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
