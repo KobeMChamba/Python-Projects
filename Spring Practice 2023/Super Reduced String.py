068f2c33627d70255882aca6d472cc894d392750
Kobe Chamba <kchamba01@gmail.com>
2023-06-13 00:00:28 -0500

    Started SRS.py

---
+++ Spring Practice 2023/Super Reduced String.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superReducedString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def superReducedString(s):
+    if s == "":
+        return "Empty String"
+    else:
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
