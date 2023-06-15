fa87bd24e6b1e511de400dfb39a920a18b4eb085
Kobe Chamba <kchamba01@gmail.com>
2023-06-14 22:40:34 -0500

    Advanced SRS.py

--- Spring Practice 2023/Super Reduced String.py
+++ Spring Practice 2023/Super Reduced String.py
@@ -12,11 +12,32 @@ import sys
 # The function is expected to return a STRING.
 # The function accepts STRING s as parameter.
 #
-
+    
 def superReducedString(s):
+    print("s: ", s)
+    original_s = s
     if s == "":
         return "Empty String"
-    else:
+    prev = None
+    ##while loop here
+    for index, char in enumerate(s):
+        print("index: ", index)
+        print("char: ", char)
+        if char == prev:
+            print("IST")
+            print("   s: ", s)
+            s = s[:(index-1)]+s[(index+1):]
+            print("   s: ", s)
+        prev = char
+    if original_s == s:
+        print("Final")
+        print("s:", s)
+        #s = s.replace(" ", "")
+        #print("s:", s)
+        print(type(s))
+        return s
+    
+    
         
 
 if __name__ == '__main__':
