ce657996588491bf523cd63fa35db9722b170d75
Kobe Chamba <kchamba01@gmail.com>
2023-09-25 21:14:31 +0900

    Completed Subarray and Subsequence.py

--- Fall Practice 2023/Max Subarray and Subsequence.py
+++ Fall Practice 2023/Max Subarray and Subsequence.py
@@ -18,9 +18,9 @@ def maxSubarray(arr):
     current_sum = arr[0]  # Initialize the current sum as the first element
     start_index = 0  # Initialize the starting index of the maximum subarray
     end_index = 0  # Initialize the ending index of the maximum subarray
-    max_non_cont_sum = arr[0]  # Initialize the maximum non-continuous sum
+    max_subseq = arr[0]  # Initialize the maximum non-continuous sum
 
-
+    print(arr)
     for i in range(1, len(arr)):
         # Update the current sum by either extending the subarray or starting a new subarray
         if arr[i] > (current_sum + arr[i]):
@@ -30,17 +30,21 @@ def maxSubarray(arr):
         else:
             #add i to the current sum
             current_sum += arr[i]
-
         # Update the maximum sum and ending index if the current sum is greater
         if current_sum > max_sum:
             #we kept adding elements, now we update the ending index if it actually increases the sum 
             max_sum = current_sum
             end_index = i
+        #now we will look for the maximum subseq
+        if (max_subseq+arr[i] > max_subseq):
+            max_subseq = max_subseq+arr[i]
+        if (arr[i] > max_subseq):
+            max_subseq = arr[i]
             
-        max_non_cont_sum = max(max_non_cont_sum, max_non_cont_sum + arr[i])
+        
 
     # Return the maximum subarray and subsequence
-    return max_sum, max_non_cont_sum
+    return max_sum, max_subseq
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
