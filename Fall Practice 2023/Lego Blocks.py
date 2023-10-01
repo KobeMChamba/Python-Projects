c0fbdbbf8004b252fef38b2ca8d82b0f33657948
Kobe Chamba <kchamba01@gmail.com>
2023-10-01 20:39:36 +0900

    Completed Lego Blocks.py

--- Fall Practice 2023/Lego Blocks.py
+++ Fall Practice 2023/Lego Blocks.py
@@ -16,11 +16,18 @@ import sys
 #
 
 def legoBlocks(n, m):
-    #define block sizes
-    block_sizes=[[1,1], [1,2], [1,3], [1,4]]
+    f = [1]
+    g = [1]
+    h = [1]
+    for i in range(1, m + 1):
+        sumhg = 0
+        for j in range(1, i):
+            sumhg += h[j] * g[i-j] % (10**9+7)
+        f.append(sum(f[-4:]) % (10**9+7))    
+        g.append(f[i] ** n % (10**9+7))
+        h.append((g[i] - sumhg) % (10**9+7))
+    return h[-1] % (10**9+7)
 
-    #create a table for how many combinations for smaller walls, initialize with zeros
-      
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
