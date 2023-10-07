d0dc30c79a40f004f7f852d5b9581f296bfb0d12
Kobe Chamba <kchamba01@gmail.com>
2023-10-07 12:57:43 +0900

    Started work on WUS

---
+++ Fall Practice 2023/Weighted Uniform Strings.py
@@ -0,0 +1,11 @@
+def weightedUniformStrings(s, queries):
+    letter = s[0]
+    counter = 1
+    letter_dict = {}
+    for i in range(1,len(s)-1):
+        if letter not in letter_dict:
+            letter_dict[letter] = counter
+        elif counter > letter_dict[letter]:
+            letter_dict[letter] = counter
+
+            
