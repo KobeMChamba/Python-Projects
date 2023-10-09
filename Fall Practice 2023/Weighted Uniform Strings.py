c13d1aa43f24ec0f222f2b9f24b55648574ba5bb
Kobe Chamba <kchamba01@gmail.com>
2023-10-09 14:20:03 +0900

    Progressed through WUS

--- Fall Practice 2023/Weighted Uniform Strings.py
+++ Fall Practice 2023/Weighted Uniform Strings.py
@@ -1,12 +1,34 @@
 def weightedUniformStrings(s, queries):
+    answers = []
     letter = s[0]
     counter = 1
     letter_dict = {}
     for i in range(1,len(s)-1):
-        if letter not in letter_dict:
-            letter_dict[letter] = counter
-        elif counter > letter_dict[letter]:
-            letter_dict[letter] = counter
+        if s[i] == letter:
+            counter = counter + 1
+        else: 
+            if letter not in letter_dict:
+                letter_dict[letter] = counter
+            elif counter > letter_dict[letter]:
+                letter_dict[letter] = counter
+    
+    
+    for q in queries:
+        q_ans = False
+        for value in letter_dict.values():
+            if divisible(q, value):
+                q_ans = True
+                break
+        answers.append(q_ans)
+    
+    return answers
+
+            
+
+def divisible(a, b):
+    if a % b == 0:
+        return True
+    return False
 
 
  
