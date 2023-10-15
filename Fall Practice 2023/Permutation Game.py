ace90ea5673dac4cc362a1796ac8f8d381a878e0
Kobe Chamba <kchamba01@gmail.com>
2023-10-15 18:36:03 +0900

    Solved Permutation Game with memoization and recursion to increase efficiency

--- Fall Practice 2023/Permutation Game.py
+++ Fall Practice 2023/Permutation Game.py
@@ -1,4 +1,4 @@
- #!/bin/python3
+#!/bin/python3
 
 import math
 import os
@@ -12,16 +12,53 @@ import sys
 # The function is expected to return a STRING.
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
-def perm(t):
-    if all(a<b for a,b in zip(t,t[1:])):
-        return 1 
-    elif any( 1 == perm(t[:i]+t[i+1:]) for i in range(len(t))):
-        return 0
-    else:
-        return 1 
-   
+
 def permutationGame(arr):
-    return 'Alice' if perm(tuple(arr)) == 0 else 'Bob'
+    #Simple lambda function that returns true if all items are in increasing order
+    isIncreasing = lambda arr: all([arr[i] < arr[i + 1] for i in range(len(arr) - 1)])
+    
+    memo = {}
+ 
+    def findWinner(arr):
+        print("findWinner")
+        print("memo: ", memo)
+        key = tuple(arr)
+        print("key: ", key)
+        if key in memo:
+            "key in memo"
+            return memo[key]
+        
+    # If arr is ascending, then this player wins (base case)
+        if isIncreasing(arr):
+            print("is increasing, adding true to memo")
+            memo[key] = True
+            return True
+    
+    # Calculate next turns: If next player has any available winning moves, this player lost
+    #You check if the player can make a move that makes the other player lose
+        for idx in range(len(arr)):
+            print("removed idx: ", arr[idx], "arr=: ", arr[:idx] + arr[idx + 1:])
+            #if findwinner, aka if next step leads to the other player winning, the previous state leads to a loss for the current player
+            if findWinner(arr[:idx] + arr[idx + 1:]):
+                print("findwinner was true")
+                print("memo[key]: ", key)
+                print("adding to memo: False")
+                memo[key] = False
+                return False
+            
+    # Otherwise, this player wins because next player has no winning moves available
+        memo[key] = True
+        print("memo[key]: ", key)
+        print("adding to memo: True")
+        return True
+   
+    
+    ans = "Bob"
+    if not findWinner(arr):
+        ans = "Alice"
+    print("returning: ", ans)
+    return ans
+    #return 'Bob' if findWinner(arr) else 'Alice'
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
