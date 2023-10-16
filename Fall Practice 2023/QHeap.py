281b5f2304d0f244b1fd54583b86664476ea20f2
Kobe Chamba <kchamba01@gmail.com>
2023-10-16 17:13:06 +0900

    Started QHeap

---
+++ Fall Practice 2023/QHeap.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+user_input = input()
+while user_input:
+    if user_input[0] == 1:
+        # add to the heap
+        
+    elif user_input[0] == 2:
+        # delete from heap
+    
+    else:
+        #print minimum of all elements
