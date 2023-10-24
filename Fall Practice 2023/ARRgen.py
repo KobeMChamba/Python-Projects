615031c867ab2ec188c065d266015ba9f491a99d
Kobe Chamba <kchamba01@gmail.com>
2023-10-24 23:21:20 +0900

    Simple random array generation function

--- Fall Practice 2023/ARRgen.py
+++ Fall Practice 2023/ARRgen.py
@@ -4,8 +4,8 @@ import random
 m = 5
 
 # Generate two arrays of random numbers between -8 and 8
-array1 = [random.randint(0, 0) for _ in range(m)]
-array2 = [random.randint(0, 0) for _ in range(m)]
+array1 = [random.randint(1, 1) for _ in range(m)]
+array2 = [random.randint(1, 1) for _ in range(m)]
 
 # Create a 2D array of their products
 product_array = [[array1[i] * array2[j] for j in range(m)] for i in range(m)]
