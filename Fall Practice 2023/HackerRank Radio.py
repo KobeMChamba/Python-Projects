32479277d4851f74fd654080500338eb063929aa
Kobe Chamba <kchamba01@gmail.com>
2023-11-02 21:55:01 +0900

    Completed HRR.py

--- Fall Practice 2023/HackerRank Radio.py
+++ Fall Practice 2023/HackerRank Radio.py
@@ -4,32 +4,17 @@ import random
 import re
 import sys
 
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-
-    return quicksort(left) + middle + quicksort(right)
-
 def hackerlandRadioTransmitters(x, k):
-    x = quicksort(x)
-    print("[0, 1, 2, 3, 4, 5, 6, 7, 8,  9,  10, 11]")
-    print(x)
-    range = 2*k
-    start = 0 
-    ant_arr = [0]
-
-    for idx, h in enumerate(x):
-        if h > x[start]+range:
-            start = idx
-            ant_arr.append(idx)
-    print(ant_arr)
-    return len(ant_arr)
-    
+    x.sort()
+    count = 0
+    while x:
+        leftHouse = x[0]
+        while x and leftHouse+k >= x[0]:
+            transmitter=x.pop(0)
+        while x and transmitter+k >= x[0]:
+            x.pop(0)
+        count += 1    
+    return count
 
 
 # Set the array x and k here
