9d7a17d8f0c8cfea1889ee16c25a771cfca8d598
Kobe Chamba <kchamba01@gmail.com>
2023-11-02 18:10:36 +0900

    HRR 2nd iteration

--- Fall Practice 2023/HackerRank Radio.py
+++ Fall Practice 2023/HackerRank Radio.py
@@ -16,46 +16,37 @@ def quicksort(arr):
     return quicksort(left) + middle + quicksort(right)
 
 def hackerlandRadioTransmitters(x, k):
-    ant_count = 0
-    ant_arr = []
-    start = 0
-    passed_ant = False
     x = quicksort(x)
+    print("[0, 1, 2, 3, 4, 5, 6, 7, 8,  9,  10, 11]")
     print(x)
-    for h_index in range(len(x)):
-        print("start")
+    h_index = 0
+    start = x[h_index]
+    end = start + 1 + 2*k
+    ant_count = 1
+    ant_arr = [0]
+    # we are at h_index = 0
+    # set end = h_index + 2k
+    # put down antenna
+    while h_index+1 < len(x):
         print("h_index: ", h_index)
-        if not passed_ant and x[h_index] >= (x[start] + k):
-            print("adding midpoint ant")
-            print("x: ", x[h_index])
-            print("midpoint: ", (x[start] + k))
-            ant_count += 1
-            ant_arr.append(h_index)
-            end = x[h_index] + k
-            passed_ant = True
-            print(ant_count)
-        elif not passed_ant and x[h_index + 1] >= (x[start] + k):
-            # next house is too far away, we need one ant for curr
-            # house
-            print("added ant for lonely house")
-            ant_count += 1
-            ant_arr.append(h_index)
-            passed_ant = True
-            end = x[h_index] + k
-        elif passed_ant and x[h_index] > end:
-            print("reached end of range")
-            start = h_index
-            passed_ant = False
-            print(ant_count)
-        if x[-1] < (x[start] + k):
-            print("reached end, adding one then break")
-            # if the remaining array is too small for full coverage
-            ant_count += 1 
-            ant_arr.append(h_index)
-            print("final count: ", ant_count)
-            break
+        if x[h_index+1] > end:
+            print("True")
+            print("end: ", end)
+            ant_arr.append(h_index+1)
+            ant_count +=1
+            h_index +=1
+            start = x[h_index]
+            end = start + 1 + 2*k
+        else:
+            #print("Else")
+            h_index +=1
+    if x[-1] > end:
+        ant_count +=1
+        ant_arr.append(len(x)+1)
     print(ant_arr)
     return ant_count
+    
+
 
 # Set the array x and k here
 x = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 14, 18]
