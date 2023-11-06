c07f49a7497c43204dbde14d13e197146fc6c685
Kobe Chamba <kchamba01@gmail.com>
2023-11-06 13:24:48 +0900

    Completed Array Manipulation

--- Fall Practice 2023/Array Manipulation.py
+++ Fall Practice 2023/Array Manipulation.py
@@ -1,45 +1,28 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'arrayManipulation' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. 2D_INTEGER_ARRAY queries
-#
-
 def arrayManipulation(n, queries):
-    ans = [0]*n
-    for idx, q in enumerate(queries):
-        print(ans)
-        for i in range(q[0]-1, q[1]):
-            ans[i]+=q[2]
-    return max(ans)
-        
+    l = []
+    for a, b, k in queries:
+        l.append((a-1, k))
+        l.append((b, -k))
+    l.sort()
+    print("l: ", l)
+    curr_sum = 0
+    best = -1
+    curr_index = 0
+    for idx, val in l:
+        if idx != curr_index:
+            best = max(best, curr_sum)
+            curr_index = idx
+        curr_sum += val
+    return best
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+n = 5  # Replace with your desired value of 'n'
 
-    first_multiple_input = input().rstrip().split()
+queries = [
+    (1, 2, 50),
+    (2, 5, 30),
+    (3, 4, 10)
+    # Add more queries as needed
+]
 
-    n = int(first_multiple_input[0])
-
-    m = int(first_multiple_input[1])
-
-    queries = []
-
-    for _ in range(m):
-        queries.append(list(map(int, input().rstrip().split())))
-
-    result = arrayManipulation(n, queries)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+result = arrayManipulation(n, queries)
+print("Maximum value:", result)
