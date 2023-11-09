3986441457118ceca24d255d0cf39c94ae061dd7
Kobe Chamba <kchamba01@gmail.com>
2023-11-09 23:25:34 +0900

    Completed Day 1: Data Types

---
+++ Fall Practice 2023/D1 Data Types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+secondInt = None
+secondDouble = None
+secondString = None
+# Read and save an integer, double, and String to your variables.
+secondInt = int(input())
+secondDouble = float(input())
+secondString = input()
+# Print the sum of both integer variables on a new line.
+print(i+secondInt)
+# Print the sum of the double variables on a new line.
+print(d+secondDouble)
+# Concatenate and print the String variables on a new line
+print(s+secondString)
+# The 's' variable above should be printed first.
