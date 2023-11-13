725f23a98f599e7684b4ce358fded9f2cbe3213f
Kobe Chamba <kchamba01@gmail.com>
2023-11-13 19:33:00 +0900

    First iteration of HVP.py

--- Fall Practice 2023/Highest Value Palindrome.py
+++ Fall Practice 2023/Highest Value Palindrome.py
@@ -17,7 +17,35 @@ import sys
 #
 
 def highestValuePalindrome(s, n, k):
-    # Write your code here
+    req_changes = 0
+    idx_to_change = []
+    idx_to_optimize = []
+    if n % 2 == 0:
+        #print((n/2-1))
+        l_range = range(int(n/2-1), -1, -1)
+        r_range = range(int(n/2), n)
+    elif n >= 3:
+        l_range = range(int((n-1)/2), -1, -1)
+        r_range = range(int((n+1)/2), n)
+    
+    indexed_values = []
+    for lidx, ridx in zip(l_range, r_range):
+        if s[lidx] != s[ridx]:
+            print("s[lidx]: ", s[lidx])
+            print("s[ridx]: ", s[ridx])
+            req_changes += 1
+            if req_changes > k:
+                return("-1")
+            idx_to_change.append((lidx, ridx))
+        else:
+            #print("matching")
+            #print(lidx)
+            indexed_values.append((lidx, int(s[lidx])))
+    #print(indexed_values)
+            #appends (value, idx)
+    sorted_indices = [index for index, value in sorted(indexed_values, key=lambda x: x[0])]
+    print("sorted_indices: ", sorted_indices)
+    return("test")
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
