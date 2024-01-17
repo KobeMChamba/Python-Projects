c080c6f568ce90a714c683bca4bad22a59bbe096
Kobe Chamba <kchamba01@gmail.com>
2024-01-16 23:57:29 -0600

    Advanced Gym Tracker

--- Gym Tracker/Gym Tracker Init.py
+++ Gym Tracker/Gym Tracker Init.py
@@ -5,19 +5,19 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 # VIDEO FEED
-cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow('Mediapipe Feed', frame)
+# cap = cv2.VideoCapture(0)
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     cv2.imshow('Mediapipe Feed', frame)
     
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+#     if cv2.waitKey(10) & 0xFF == ord('q'):
+#         break
         
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
 
 cap = cv2.VideoCapture(0)
-## Setup mediapipe instance
+## Setup mediapipe instance, set confidences
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
         ret, frame = cap.read()
@@ -29,7 +29,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         # Make detection
         results = pose.process(image)
     
-        # Recolor back to BGR
+        # Recolor back to BGR so we can output it
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
