004a6b2545118e4ab3ddf396d1daca4d12d5b15b
Kobe Chamba <kchamba01@gmail.com>
2024-02-15 15:50:56 -0600

    First attempt

---
+++ Winter Practice 2024/80. Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # Initialize pointers
+        i = 0
+        j = 1
+        j_count = 1
+        while j < len(nums):
+            print("i: ", i, " | j: ", j, "jc: ", j_count)
+            if nums[i] == nums[j]:
+                j_count += 1
+                if j_count > 2:
+                    nums.pop(j)
+                    print("nums: ", nums)
+                    j_count = 1
+                    # we removed an index so we dont have to go forward as much
+                    j -= 1
+            else:
+                j_count = 1
+                i = j
+            j +=1
+        return
