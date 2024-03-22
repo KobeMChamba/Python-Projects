a1d42123188a093ae61dfd924ff965906c425968
Kobe Chamba <kchamba01@gmail.com>
2024-03-22 13:22:42 -0400

    Completed 134. Gas Station

---
+++ Winter Practice 2024/134. Gas Station.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost): return -1
+        station = 0
+        Sum = 0
+        
+        for i in range(len(gas)):
+            Sum += gas[i] - cost[i]
+            if Sum < 0:
+                Sum = 0
+                station = i + 1
+        
+        return station
+                
