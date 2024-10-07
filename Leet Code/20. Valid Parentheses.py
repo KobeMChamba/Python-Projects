e33997e014bc5bfdcc5cc7ce0b18b33c2bb15a27
Kobe Chamba <77932095+KobeMChamba@users.noreply.github.com>
2024-10-07 15:33:24 -0400

    Completed 20. Valid Parentheses

--- Leet Code/20. Valid Parentheses.py
+++ Leet Code/20. Valid Parentheses.py
@@ -1,14 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        opp_dict = {"}": "{", "]": "[", ")": "("}
         for p in s:
+            # print("p: ", p)
             if p == "{" or p == "(" or p == "[":
                 stack.append(p)
             elif p == "}" or p == ")" or p == "]":
                 if stack == []:
+                    # print("empty")
                     return False
-                elif p == stack[-1]:
+                elif opp_dict[p] == stack[-1]:
+                    # print("match")
                     stack.pop()
                 else:
                     return False
-        return True
+        if stack == []:
+            return True
+        return False
